e32b6739d98e200f8b8f6154d785383582f07a9f
Sean Michael <sean@303software.com>
2015-04-11 18:08:51 -0600

    finally angular

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name cultureVultureApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the cultureVultureApp
+ */
+angular.module('cultureVultureApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
